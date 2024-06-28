9e5f0dbdfa39c4ce84f91fa4429091f43042331b
Tanmay6696 <tanmaydahale111@gmail.com>
2024-06-28 21:40:35 +0530

    lec9 utils code disucssion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import ConnectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
     path:'./env'
 })
 ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log("server is running " ,process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB FAILED");
+})
